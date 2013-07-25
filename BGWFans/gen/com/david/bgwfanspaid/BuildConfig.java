f4717c273c704993871c51234e885a1d2de0758d
David Hodge <david.hodge@willowtreeapps.com>
2013-07-24 23:13:47 -0400

    Remove old folder
    more clean ups

--- BGWFans/gen/com/david/bgwfanspaid/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package com.david.bgwfanspaid;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
