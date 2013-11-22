14e569692b1556569e8e57cb6d77b6f0f1909b86
David Hodge <david.hodge@willowtreeapps.com>
2013-11-22 14:33:18 -0500

    christmastown

--- src/com/david/bgwfanspaid/xmas/shows/DeckTheHalls.java
+++ src/com/david/bgwfanspaid/xmas/shows/DeckTheHalls.java
@@ -101,12 +101,9 @@ public class DeckTheHalls extends RoboSherlockFragmentActivity {
     }
 
     private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
             mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
                     .getMap();
-            // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap();
             }
